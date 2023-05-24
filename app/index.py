c9e86e5256e2c305b6f4fd86177fc4ccf53ee28a
czaraugusto1 <65928485+czaraugusto1@users.noreply.github.com>
2023-05-24 18:34:41 -0300

    segundo commit

--- app/index.py
+++ app/index.py
@@ -1,1 +1,23 @@
-print("eu consegui")
+import pika
+
+# Função para gerar mensagens
+def gerar_mensagens():
+    # Conexão com o RabbitMQ
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Cria a fila
+    channel.queue_declare(queue='fila_teste')
+
+    # Gera 10 mensagens para serem consumidas
+    for i in range(1, 11):
+        mensagem = f"Mensagem {i}"
+        # Publica a mensagem na fila
+        channel.basic_publish(exchange='', routing_key='fila_teste', body=mensagem)
+        print("Mensagem gerada e enviada: %r" % mensagem)
+
+    # Fecha a conexão
+    connection.close()
+
+# Chamada da função para gerar mensagens
+gerar_mensagens()
